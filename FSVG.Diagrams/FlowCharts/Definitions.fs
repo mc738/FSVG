efa749013afc49de8a4a2766ddef72cba82c23e8
Max Clifford <me@mclifford.dev>
2023-12-21 18:18:08 +0000

    Added Definitions.fs

---
+++ FSVG.Diagrams/FlowCharts/Definitions.fs
@@ -0,0 +1,43 @@
+﻿namespace FSVG.Diagrams.FlowCharts
+
+// No warning needed for use of DiagramNode and Diagram,
+// they are only used for internal purposes here.
+#nowarn "10001"
+
+module Definitions =
+
+    open FSVG.Diagrams.Common.Shared
+
+    type FlowChartNode =
+        { Id: string
+          Class: FlowChartNodeClass }
+
+        member fcn.ToNode() =
+            ({ Id = fcn.Id
+               Connections = []
+               Class = Some <| fcn.Class.Serialize() }
+            : Definitions.DiagramNode)
+
+    and FlowChartNodeConnection =
+        { ToId: string }
+
+        member fnc.ToNodeConnection() =
+            Definitions.DiagramNodeConnection.Create(fnc.ToId)
+
+    and [<RequireQualifiedAccess>] FlowChartNodeClass =
+        | Rectangle
+        | Unknown of Name: string
+
+        static member TryDeserialize(str: string) =
+            match str.ToLower() with
+            | "rectangle" -> Some FlowChartNodeClass.Rectangle
+            | _ -> None
+
+        static member Deserialize(str: string) =
+            FlowChartNodeClass.TryDeserialize str
+            |> Option.defaultValue (FlowChartNodeClass.Unknown str)
+
+        member fnc.Serialize() =
+            match fnc with
+            | Rectangle -> "rectangle"
+            | Unknown v -> v
