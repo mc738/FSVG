5bb66b7a244ad8dd681b05d0d6bf6eef762e779d
Max Clifford <me@mclifford.dev>
2023-12-21 18:19:01 +0000

    Added LayeredGraphDrawing.fs

---
+++ FSVG.Diagrams/Common/Layout/LayeredGraphDrawing.fs
@@ -0,0 +1,10 @@
+﻿namespace FSVG.Diagrams.Common.Layout
+
+module LayeredGraphDrawing =
+    
+    // Implementation based on https://en.wikipedia.org/wiki/Layered_graph_drawing
+    
+    // https://www.baeldung.com/cs/graph-auto-layout-algorithm
+    
+    ()
+ 
