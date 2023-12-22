3d447f0e202cda37ec6c3af330e76fa40ae7d6c7
Max Clifford <me@mclifford.dev>
2023-12-22 11:22:31 +0000

    Work on LayeredGraphDrawing.fs

--- FSVG.Diagrams/Common/Layout/LayeredGraphDrawing.fs
+++ FSVG.Diagrams/Common/Layout/LayeredGraphDrawing.fs
@@ -3,6 +3,7 @@
 // No warning needed for use of DiagramNode and Diagram, internal usage.
 #nowarn "10001"
 
+open System
 open FSVG.Diagrams.Common.Shared
 
 [<RequireQualifiedAccess>]
@@ -14,31 +15,120 @@ module LayeredGraphDrawing =
 
     // https://www.baeldung.com/cs/graph-auto-layout-algorithm
 
-    type Settings = {
-        NormalizeNodes: bool
-        NodeLimit: int option
-    }
-    
-    type Parameters =
+    type Settings =
         {
-            Nodes: Definitions.DiagramNode list
-            Settings: Settings
-        }
-        
-    type Layer =
-        {
-            Nodes: Definitions.DiagramNode
-            Order: int
+            NormalizeNodes: bool
+            /// <summary>
+            /// A optional node limit. This can prevent process excess size for performance reasons.
+            /// </summary>
+            NodeLimit: int option
+            StringComparison: StringComparison
+            StrictMode: bool
         }
 
+    type Parameters =
+        { Nodes: Definitions.DiagramNode list
+          Settings: Settings }
+
+    type Layer =
+        { Nodes: Definitions.DiagramNode
+          Order: int }
+
+
+    /// <summary>
+    /// An internal node, this reverses the connections to store a record of
+    /// </summary>
+    type InternalNode =
+        { Node: Definitions.DiagramNode
+          ConnectionsFrom: string list }
+
+    let createInternalNodes (parameters: Parameters) =
+        // PERFORMANCE Could this be optimized?
+        parameters.Nodes
+        |> List.map (fun n ->
+
+            { Node = n
+              ConnectionsFrom =
+                parameters.Nodes
+                |> List.filter (fun on ->
+                    on.Connections
+                    |> List.exists (fun nc -> nc.ToId.Equals(n.Id, parameters.Settings.StringComparison)))
+                |> List.map (fun on -> on.Id) })
     
-    let createLayers (parameters: Parameters) =
-        let rec handler1 () =
-            ()
+    type VerificationResultItem = { FromNode: string; ToNode: string }
+    
+    type VerificationResult =
+        | Success of VerificationResultItem
+        | CyclicReference of VerificationResultItem
+        | NodeNotFound of VerificationResultItem
+
+    type VerificationResults =
+        { Successes: VerificationResultItem list
+          CyclicReferences: VerificationResultItem list
+          NodesNotFound: VerificationResultItem list }
         
+        //static member Create(results: VerificationResult list) =
+            
+
+    let contains (comparison: StringComparison) (value: string) (values: string list) =
+        values |> List.exists (fun v -> v.Equals(value, comparison))
+
+    let verifyInternalNodes (parameters: Parameters) (nodes: InternalNode list) =
+        // For go FP for the sake of internal usage.
+        // This are oversize, but in practice that shouldn't matter,
+        // this is just a optimization to make the verification process a bit quicker/less resource intensive.
+        // This uses nodes.Length / 2 for the second 2 because in general there should be less of these.
+        
+        let successes = ResizeArray<VerificationResultItem>(nodes.Length)
+        let cyclicReferences = ResizeArray<VerificationResultItem>(nodes.Length / 2)
+        let nodesNotFound = ResizeArray<VerificationResultItem>(nodes.Length /  2)
+        
+        //    results |> List.iter (fun r ->
+        //        match r with
+        //        | Success vri -> s.Add vri
+        //        | CyclicReference vri -> cr.Add vri
+        //        | NodeNotFound vri -> nnf.Add vri)
+            
+            
+        
+        
+        let nodesMap = nodes |> List.map (fun n -> n.Node.Id, n) |> Map.ofList
+
+        nodes
+        |> List.collect (fun n ->
+            n.ConnectionsFrom
+            |> List.map (fun ncf ->
+                match nodesMap.TryFind ncf with
+                | Some fn ->
+                    match fn.ConnectionsFrom |> contains parameters.Settings.StringComparison n.Node.Id with
+                    | true ->
+                        { FromNode = fn.Node.Id
+                          ToNode = n.Node.Id }
+                        |> cyclicReferences.Add
+                        
+                        
+                        VerificationResult.CyclicReference(n.Node.Id, fn.Node.Id)
+                    | false -> VerificationResult.Success(n.Node.Id, fn.Node.Id)
+                | None -> VerificationResult.NodeNotFound(n.Node.Id, ncf)))
+
+
+
+    let createLayers (parameters: Parameters) =
+        let rec handler1 (layers) = ()
+
+
+
+
         ()
-     
+
     let handle (parameters: Parameters) =
+
+        // Pre - create and verify internal nodes.
+        let internalNodes = createInternalNodes parameters
+
+        let verificationResults = verifyInternalNodes parameters internalNodes
+
+
 
         // First create a directed acyclic graph
         // This also needs to verify data to make sure cycles don't exist.
@@ -46,14 +136,14 @@ module LayeredGraphDrawing =
 
         // Next create layers, the goal is to minimise the number of layers
         // and limit the number of that span multiple layers
-        
+
         // Then we can start working out placement within layers and connections
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
         ()
 
 
