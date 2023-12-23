cfb6fe7aa596092d563535f413ff1481af6476b0
Max Clifford <me@mclifford.dev>
2023-12-23 11:37:55 +0000

    Added FSVG.Diagrams.Tests.fsproj 3

---
+++ FSVG.Diagrams.Tests/Tests.fs
@@ -0,0 +1,11 @@
+namespace FSVG.Diagrams.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
