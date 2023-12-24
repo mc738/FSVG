2f8c23f466f4b5f72b52683794038c3a2b471ddf
Max Clifford <me@mclifford.dev>
2023-12-24 11:54:30 +0000

    More work on LayeredGraphDrawingTests.fs

--- FSVG.Diagrams.Tests/Common/Layout/LayeredGraphDrawingTests.fs
+++ FSVG.Diagrams.Tests/Common/Layout/LayeredGraphDrawingTests.fs
@@ -2,6 +2,7 @@
 
 open System
 open FSVG.Diagrams.Common.Layout
+open FSVG.Diagrams.Common.Shared
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
@@ -10,37 +11,47 @@ type LayeredGraphDrawingTests() =
     [<TestMethod>]
     member _.``createLayers basic test``() =
 
+        let nodeA =
+            ({ Id = "node_a"
+               Connections =
+                 [ { ToId = "node_b"
+                     TwoWay = false
+                     Class = None } ]
+               Class = None }
+            : Definitions.DiagramNode)
+
+        let nodeB =
+            ({ Id = "node_b"
+               Connections =
+                 [ { ToId = "node_c"
+                     TwoWay = false
+                     Class = None } ]
+               Class = None }
+            : Definitions.DiagramNode)
+
+        let nodeC =
+            ({ Id = "node_c"
+               Connections =
+                 [ { ToId = "node_a"
+                     TwoWay = false
+                     Class = None } ]
+               Class = None }
+            : Definitions.DiagramNode)
+
+
         let nodes: LayeredGraphDrawing.InternalNode list =
-            [ { Node =
-                  { Id = "node_a"
-                    Connections =
-                      [ { ToId = "node_b"
-                          TwoWay = false
-                          Class = None } ]
-                    Class = None }
+            [ { Node = nodeA
                 PreferredOrder = 0
                 ConnectionsFrom = [ "node_c" ] }
-              { Node =
-                  { Id = "node_b"
-                    Connections =
-                      [ { ToId = "node_c"
-                          TwoWay = false
-                          Class = None } ]
-                    Class = None }
+              { Node = nodeB
                 PreferredOrder = 0
                 ConnectionsFrom = [ "node_a" ] }
-              { Node =
-                  { Id = "node_c"
-                    Connections =
-                      [ { ToId = "node_a"
-                          TwoWay = false
-                          Class = None } ]
-                    Class = None }
+              { Node = nodeC
                 PreferredOrder = 0
                 ConnectionsFrom = [ "node_b" ] } ]
 
         let parameters =
-            ({ Nodes = []
+            ({ Nodes = [ nodeA; nodeB; nodeC ]
                Settings =
                  { NormalizeNodes = true
                    NodeLimit = Some 1024
@@ -49,6 +60,6 @@ type LayeredGraphDrawingTests() =
                    PreferredOrderHandler = None } }
             : LayeredGraphDrawing.Parameters)
 
-        let actual = LayeredGraphDrawing.createLayers
+        let actual = LayeredGraphDrawing.createLayers parameters nodes
 
         ()
