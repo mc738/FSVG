52b17c99dd1e78220c21fd709cb09617caa5ea7b
Max Clifford <me@mclifford.dev>
2023-12-25 11:25:59 +0000

    Work on Common.fs

---
+++ FSVG.Diagrams/Common/Rendering/Common.fs
@@ -0,0 +1,33 @@
+﻿namespace FSVG.Diagrams.Common.Rendering
+
+// No warning needed for use of DiagramNode and Diagram
+#nowarn "10001"
+
+[<AutoOpen>]
+module Common =
+
+    open FSVG.Diagrams.Common.Shared
+
+    type RenderingType = | Grid
+
+    and GridRenderingSettings =
+        { Rows: GridRow list }
+
+        member grs.RowCount() = grs.Rows.Length
+
+        member grs.ColumnCount() =
+            grs.Rows |> List.maxBy (fun r -> r.ColumnCount()) |> (fun r -> r.ColumnCount())
+
+    and GridRow =
+
+        { Order: int
+          Nodes: GridNode list }
+
+        member gr.ColumnCount() = gr.Nodes.Length
+
+    and GridNode =
+        { Node: Definitions.DiagramNode
+          Column: int }
+
+
+    ()
