d23f45b4554c04bc22e953ed63202c1aa50b64d7
Max Clifford <me@mclifford.dev>
2023-12-28 18:16:10 +0000

    Added properties

--- FSVG.Diagrams/Common/Shared/Rendering.fs
+++ FSVG.Diagrams/Common/Shared/Rendering.fs
@@ -3,6 +3,8 @@
 // No warning needed for use of DiagramNode and Diagram
 #nowarn "10001"
 
+open System
+
 module Rendering =
 
     open FSVG.Diagrams.Common.Shared
@@ -16,11 +18,20 @@ module Rendering =
         | Fixed of float
         | Percentage of float
         | Dynamic
-    
+
     and GridRendererSettings =
         { Rows: GridRow list
           Properties: Map<string, string>
           Classes: string list }
+
+        member grs.TryGetProperty(key: string) = grs.Properties.TryFind key
+
+        member grs.TryGetPropertyAsFloat(key: string) =
+            grs.TryGetProperty key
+            |> Option.bind (fun p ->
+                match Double.TryParse p with
+                | true, v -> Some v
+                | false, _ -> None)
 
         member grs.RowCount() = grs.Rows.Length
 
@@ -43,9 +54,5 @@ module Rendering =
     let run (settings: RendererSettings) =
         match settings with
         | Grid gridRendererSettings ->
-            
-            
-            
+
             gridRendererSettings.Rows
-            
-        
